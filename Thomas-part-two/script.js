8330d27d5d3e7b0d4eadb97db296ba3028910886
KaylaLT <72283345+KaylaLT@users.noreply.github.com>
2020-10-08 18:32:37 -0500

    Add files via upload

---
+++ Thomas-part-two/script.js
@@ -0,0 +1,42 @@
+function fertilizer(){
+          let fertilizer = prompt ("do you use fertilizer? Yes or No");
+          if (fertilizer = yes)
+          {
+               let yes = prompt ("what type of fertizler do you use? Premium or Regular");
+          }
+          if yes = "Premium" {
+          alert(yes + "means add 15% to the yield")
+          }
+          if  (yes = "Regular"){
+                  alert(yes + "means add 10% to the yield")
+          }
+          if (fertilizer= no){
+          alert (no + "means do not change the yield.")
+          }
+      }
+          function rain() {
+          let rain = prompt("How many inches of rain?");
+          if (rain >=20 )
+              alert("There's too much rain, the number of bushels goes down by 10%");
+          } if (rain <= 10)
+              alert("Theres too little rain, the number of bushles goes down by 20%");
+          } else{ alert("There's 50 bushels of grain")}
+      }
+rain ();
+}
+    let rain = [20, 10]
+    for (let i = 0; i < rain.bushles; i++) {
+        yield rain]
+    }
+}
+document.addEventListener(bushles, function(rain) {
+newFunction();
+
+function newFunction() {
+    const iterator = bushles(0);
+    console.log(iterator.next().value);
+    // expected output: <=10
+    console.log(iterator.next().value);
+// expected output: >=20
+}
+}
