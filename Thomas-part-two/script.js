a7b6465f2acf2b7a7386aa3232e20ee4387c7137
KaylaLT <72283345+KaylaLT@users.noreply.github.com>
2020-12-16 14:54:17 -0600

    Add files via upload

--- Thomas-part-two/script.js
+++ Thomas-part-two/script.js
@@ -1,42 +1,43 @@
-function fertilizer(){
-          let fertilizer = prompt ("do you use fertilizer? Yes or No");
-          if (fertilizer = yes)
-          {
-               let yes = prompt ("what type of fertizler do you use? Premium or Regular");
-          }
-          if yes = "Premium" {
-          alert(yes + "means add 15% to the yield")
-          }
-          if  (yes = "Regular"){
-                  alert(yes + "means add 10% to the yield")
-          }
-          if (fertilizer= no){
-          alert (no + "means do not change the yield.")
-          }
-      }
-          function rain() {
-          let rain = prompt("How many inches of rain?");
-          if (rain >=20 )
-              alert("There's too much rain, the number of bushels goes down by 10%");
-          } if (rain <= 10)
-              alert("Theres too little rain, the number of bushles goes down by 20%");
-          } else{ alert("There's 50 bushels of grain")}
-      }
-rain ();
-}
-    let rain = [20, 10]
-    for (let i = 0; i < rain.bushles; i++) {
-        yield rain]
-    }
-}
-document.addEventListener(bushles, function(rain) {
-newFunction();
+let rain = window.prompt ("How many inches of rain fell?");
 
-function newFunction() {
-    const iterator = bushles(0);
-    console.log(iterator.next().value);
-    // expected output: <=10
-    console.log(iterator.next().value);
-// expected output: >=20
+function repeatStringNumTimes (str,) {
+    return str.repeat( rain)
 }
+console.log (repeatStringNumTimes("*", rain)); 
+
+const startBushelYield = 50; 
+let finalYield = null; 
+
+let bushelYield = null;
+let preFert = ("premium");
+let regFert = ("regular");
+
+
+if  (rain >= 20){ 
+    bushelYield = (startBushelYield * 0.9); 
 }
+else if (rain < 10) { 
+    bushelYield = (startBushelYield * 0.8);
+}
+else {
+    bushelYield = startBushelYield;
+}
+
+let fert = window.prompt("Did you use fertilizer?");
+if (fert == "yes") {
+let fertType = window.prompt("Did you use premium or regular fertilizer? ");
+}
+
+// my fertizler is not adjusting the yield in the console help needed here 
+
+if (fertType =  preFert) {
+    finalYield = bushelYield  * 1.15;
+}
+else if (fertType = regFert) { 
+    finalYield =  bushelYield  * 1.1;
+}
+else ( fertType = null);  {
+    finalYield = bushelYield
+}
+
+console.log ("The yield should be "  + finalYield + " bushels per acre"); 
